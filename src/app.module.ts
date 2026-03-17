ffa7caa4e788fc4ed85c9f636aff72b80a44d27a
MaxiGomez5431 <mamitgomez@gmail.com>
2026-03-16 21:11:28 -0300

    auction resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ArtworkModule } from "./artwork/artwork.module";
+import { AuctionModule } from "./auction/auction.module";
 
 @Module({
-  imports: [PrismaModule, ArtworkModule],
+  imports: [PrismaModule, ArtworkModule, AuctionModule],
   controllers: [AppController],
   providers: [AppService],
 })
